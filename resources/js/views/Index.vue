2ef8ad3f442fc2469c74486bd518e60d04537213
VolodymyrVi <sqlphpjs@gmail.com>
2021-06-10 15:02:10 +0300

    added views/Index.vue

---
+++ resources/js/views/Index.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <h1>
+            Home page
+        </h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Index"
+}
+</script>
+
+<style scoped>
+
+</style>
