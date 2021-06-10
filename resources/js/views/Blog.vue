b77c0d8ea5ac451b14ee3a0008c176ffdf630d04
VolodymyrVi <sqlphpjs@gmail.com>
2021-06-10 15:40:47 +0300

    added Blog.vue

---
+++ resources/js/views/Blog.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <h1>
+            Blog page
+        </h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Blog"
+}
+</script>
+
+<style scoped>
+
+</style>
